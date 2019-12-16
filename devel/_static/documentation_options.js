8c39925fca87dfb07f7babdf80fbd886ec175daf
thequackdaddy <pquack@gmail.com>
2019-12-15 19:13:42 -0600

    Add devel

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.11.0dev0+691.g89fa4e3b6',
+    VERSION: 'v0.11.0dev0+691.g89fa4e3b6.dirty',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
